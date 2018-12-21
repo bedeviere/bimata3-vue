1513be0e805a2526aa280dde4ab5b4d3c67a142b
Bimata Prathama <bimata.prathama@gmail.com>
2018-12-22 04:34:07 +0700

    added vue analytics

--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ import 'bulma/css/bulma.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+const isProd = process.env.NODE_ENV === 'production'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-62609356-1',
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+})     
 
 new Vue({
   router,
